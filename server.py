851d3f20c3866bd9af840025995f024a5c36d414
Liang Bo Wang <ccwang002@gmail.com>
2015-05-30 11:33:36 +0800

    add list questions page

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
 from bottle import Bottle, jinja2_view, run, abort, static_file, request
+from functools import partial
+from pathlib import Path
+import re
 
 app = Bottle()
+jinja2_template = partial(jinja2_view, template_lookup=['templates'])
+
 
 @app.route('/', method='GET')
-@jinja2_view('index.html', template_lookup=['templates'])
+@jinja2_template('index.html')
 def index(msg=''):
     return {'msg': msg}
+
+
+@app.route('/question/', method='GET')
+@jinja2_template('questions.html')
+def list_question():
+    questions = Path('questions').glob('q_*.py')
+    extract_q_name = re.compile('^q_(.+)$').match
+
+    return {
+        'questions': [extract_q_name(p.name).group(1) for p in questions]
+    }
+
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True, reloader=True)
