0763ecea2431dd703ebf1158c18cafb3b3d6b43f
Liang Bo Wang <ccwang002@gmail.com>
2015-05-30 11:19:46 +0800

    bootstrap into index page

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from bottle import Bottle, jinja2_view, run, abort, static_file, request
 
 app = Bottle()
 
-@app.route('/')
-def landing_page():
-    return 'Welcome to Code Fighter!'
+@app.route('/', method='GET')
+@jinja2_view('index.html', template_lookup=['templates'])
+def index(msg=''):
+    return {'msg': msg}
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True, reloader=True)
