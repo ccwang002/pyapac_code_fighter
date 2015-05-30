a0ff522c0570df616c9b36cbb53c063b186b57d3
AlfredWei <Wei.Alfred@gmail.com>
2015-05-30 12:09:06 +0800

    [what] add raw function

--- server.py
+++ server.py
@@ -23,6 +23,20 @@ def list_question():
         'questions': [extract_q_name(p.name).group(1) for p in questions]
     }
 
+@app.route('/play/', method='GET')
+@jinja2_template('play.html')
+def play():
+    return {'msg': 'play'}
+
+@app.route('/judge/', method='GET')
+@jinja2_template('judge.html')
+def judge():
+    return {'msg': 'judge'}
+
+@app.route('/admin/', method='GET')
+@jinja2_template('admin.html')
+def admin():
+    return {'msg': 'admin'}
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True, reloader=True)
