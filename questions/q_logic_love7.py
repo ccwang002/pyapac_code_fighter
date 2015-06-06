40587281315c7c5d9df43df5cebc8a7a04b9f7a8
AlfredWei <Wei.Alfred@gmail.com>
2015-06-06 14:59:58 +0800

    [what] commit  question  love 7

---
+++ questions/q_logic_love7.py
@@ -0,0 +1,42 @@
+'''Question logic_love7: (copyright -> CodingBat)
+
+
+Description
+-----------
+
+
+The number 7 is a truly great lucky number. Given two int values, a and b, 
+return True if either one is 7. Or if their sum or difference is 6. 
+
+
+Examples
+--------
+
+
+'''
+
+def answer(a, b):
+
+    return True
+
+
+def test_answer_ex1():
+    test = ( 1, 8 )
+    assert answer(*test) == True
+    
+
+def test_answer_ex2():
+    test = ( 7 , 8 )
+    assert answer(*test) == True
+
+def test_answer_ex3():
+    test = ( -4 , 11 )
+    assert answer(*test) == True
+    
+def test_answer_ex4():
+    test = ( 99 , 91 )
+    assert answer(*test) == True
+    
+def test_answer_ex5():
+    test = ( 1, 3 )
+    assert answer(*test) == False
